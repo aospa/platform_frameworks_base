5d625331aafbbe88cc116bdc6a021216ce74c168
Carlo Savignano <carlosavignano@aospa.co>
2017-03-29 15:09:00 +0100

    base: Add gestures control [1/2]
    * Database policy
    * KeyHandler for gestures inputs
    * Input policy integration
    * Supported ALL QCOM chipsets synaptic gestures
    
    Relies on our AOSPA PowerHAL
    
    Change-Id: I84aa3bcb6dc5f8b003423b5a5af3ec4e43578fde
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Co-Authored-By: Alexander Koskovich <zvnexus@outlook.com>
    Co-Authored-By: 33bca <fabian.leutenegger@bluewin.ch>
    Co-Authored-By: Willi Ye <williye97@gmail.com>
    Co-Authored-By: TheStrix <parthbhatia98@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -705,6 +705,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
+    // Gesture key handler.
+    private KeyHandler mKeyHandler;
+
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -2570,6 +2573,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         if (DEBUG_INPUT) {
             Slog.d(TAG, "" + mDeviceKeyHandlers.size() + " device key handlers loaded");
+        }
+        boolean enableKeyHandler = mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_enableKeyHandler);
+        if (enableKeyHandler) {
+            mKeyHandler = new KeyHandler(mContext);
         }
     }
 
@@ -4797,6 +4805,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
+        /**
+         * Handle gestures input earlier then anything when screen is off.
+         */
+        if (!interactive) {
+            if (mKeyHandler != null && mKeyHandler.handleKeyEvent(event)) {
+                return 0;
+            }
+        }
+
         // Pre-basic policy based on interactive and pocket lock state.
         if (mIsDeviceInPocket && (!interactive || mPocketLockShowing)) {
             if (keyCode != KeyEvent.KEYCODE_POWER &&
@@ -5101,7 +5118,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_WAKEUP: {
                 logKeyboardSystemsEventOnActionUp(event, KeyboardLogEvent.WAKEUP);
                 result &= ~ACTION_PASS_TO_USER;
-                isWakeKey = true;
+                isWakeKey = false;  // We handle this in KeyHandler
                 break;
             }
 
@@ -6277,6 +6294,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
+        if (mKeyHandler != null) {
+            mKeyHandler.systemReady();
+        }
     }
 
     /** {@inheritDoc} */
